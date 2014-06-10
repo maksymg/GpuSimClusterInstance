e921db32b023681a57358b433c85b3d7ed2fef9c
maksymg <mgnyniuk@gmail.com>
2014-06-10 16:30:09 +0300

    update comments to console

--- src/main/java/com/mgnyniuk/core/parallel/WorkerThread.java
+++ src/main/java/com/mgnyniuk/core/parallel/WorkerThread.java
@@ -1,5 +1,8 @@
 package com.mgnyniuk.core.parallel;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by maksym on 5/24/14.
  */
@@ -19,6 +22,15 @@ public class WorkerThread extends NotifyingThread {
         try {
             Process process = Runtime.getRuntime().exec("java -jar " + runningJar + " " + configFile + " " + outputFile);
             System.out.println("Process with " + configFile + " is started: " + process + "==== Start time: " + System.currentTimeMillis());
+
+            BufferedReader gpuSimV2Output;  // reader for output of process
+            String line;
+
+            gpuSimV2Output = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            while ((line = gpuSimV2Output.readLine()) != null)
+                System.out.println(line);
+
             process.waitFor();
             System.out.println("Exit value of process with " + configFile + ": " + process.exitValue() + "==== Exit time: " + System.currentTimeMillis());
         } catch (Exception ex) {
